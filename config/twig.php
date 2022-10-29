acb54c84933f80ba509729efde8c72a3a093d5d7
chartier dimitri <chartier.dimitri1@gmail.com>
2022-10-29 18:25:09 +0200

    exercice

--- config/twig.php
+++ config/twig.php
@@ -1,7 +1,7 @@
 <?php
 
 require_once __DIR__ . '/../vendor/autoload.php';
-
+// tu indiques ici à Twig où aller chercher tes fichiers de vues
 $loader = new Twig\Loader\FilesystemLoader(__DIR__ . '/../src/View');
 $twig = new Twig\Environment($loader, ['debug' => true]);
 $twig->addExtension(new Twig\Extension\DebugExtension());
